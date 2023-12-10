5791abcab768a9f484a9cc8a6929c9df7f15ea32
JJongW <shinjw4675@gmail.com>
2023-12-10 16:21:46 +0900

    [NON_ISSUE] 초기 구성

---
+++ Assignment3/Assignment3/Assignment3App.swift
@@ -0,0 +1,18 @@
+//
+//  Assignment3App.swift
+//  Assignment3
+//
+//  Created by 신종원 on 12/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Assignment3App: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment3/Assignment3/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Assignment3
+//
+//  Created by 신종원 on 12/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    var body: some View {
+        VStack {
+            Text("dddd")
+            Text("dddd")
+            HStack(alignment: .center) {
+                Text("dfds")
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
